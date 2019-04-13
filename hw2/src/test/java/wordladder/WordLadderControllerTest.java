58900824523f013ce8cf166f6787319a8e50fa0e
bald-win <1351533120@qq.com>
2019-04-14 00:07:03 +0800

    mockmvc 用的junit4 前一个test需要重构

---
+++ hw2/src/test/java/wordladder/WordLadderControllerTest.java
@@ -0,0 +1,28 @@
+package wordladder;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@WebAppConfiguration
+public class WordLadderControllerTest {
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Before()
+    public void setup()
+    {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();  //初始化MockMvc对象
+    }
+
+}
