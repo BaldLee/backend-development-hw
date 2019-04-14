56b804a5ad869cfc7061b9c673de7d8913f10b8a
bald-win <1351533120@qq.com>
2019-04-14 11:23:21 +0800

    集成测试通过

--- hw2/src/test/java/wordladder/WordLadderControllerTest.java
+++ hw2/src/test/java/wordladder/WordLadderControllerTest.java
@@ -15,8 +15,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -30,21 +29,111 @@ public class WordLadderControllerTest {
     protected WebApplicationContext wac;
 
     @BeforeEach()
-    public void setup()
-    {
-
+    public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
     @Test
     @DisplayName("test1")
-    public void test1() throws Exception
-    {
+    public void test1() throws Exception {
         this.mockMvc.perform(
                 get("/wladder")
                 .contentType("application/json;charset=UTF-8")
                 )
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$", Matchers.contains("cat","cot","dot","dog"))));
+    }
+
+    @Test
+    @DisplayName("test2")
+    public void test2() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","cat")
+                        .param("endword","log")
+        )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("cat","cot","lot","log"))));
+    }
+
+    @Test
+    @DisplayName("test3")
+    public void test3() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","pie")
+                        .param("endword","sad")
+        )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("pie","die","did","dad","sad"))));
+    }
+
+    @Test
+    @DisplayName("test4")
+    public void test4() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","sun")
+                        .param("endword","dad")
+        )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("sun","dun","dud","dad"))));
+    }
+
+    @Test
+    @DisplayName("test5")
+    public void test5() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","soft")
+                        .param("endword","ware")
+        )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("soft","sort","wort","wart","ware"))));
+    }
+
+    @Test
+    @DisplayName("test6")
+    public void test6() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","boat")
+                        .param("endword","word")
+        )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("boat","bort","wort","word"))));
+    }
+
+    @Test
+    @DisplayName("test7")
+    public void test7() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","sound")
+                        .param("endword","apple")
+        )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("sound","pound","poind","point","print","prink",
+                                                                      "prick","price","prise","arise","anise","anile",
+                                                                      "anole","amole","ample","apple"))));
+    }
+
+    @Test
+    @DisplayName("test8")
+    public void test8() throws Exception {
+        this.mockMvc.perform(
+                get("/wladder")
+                        .contentType("application/json;charset=UTF-8")
+                        .param("startword","as")
+                        .param("endword","set")
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
     }
 }
