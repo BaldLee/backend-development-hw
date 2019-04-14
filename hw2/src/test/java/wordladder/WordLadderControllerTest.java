361dcbca054b2afbddab7160e84c5e2d70548133
bald-win <1351533120@qq.com>
2019-04-14 10:54:12 +0800

    通过了第一个测试

--- hw2/src/test/java/wordladder/WordLadderControllerTest.java
+++ hw2/src/test/java/wordladder/WordLadderControllerTest.java
@@ -1,20 +1,25 @@
 package wordladder;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.MockMvcBuilderSupport.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 @WebAppConfiguration()
 @AutoConfigureMockMvc
 
@@ -33,8 +38,13 @@ public class WordLadderControllerTest {
 
     @Test
     @DisplayName("test1")
-    public void test1()
+    public void test1() throws Exception
     {
-        mockMvc.perform(get("/wladder").content)
+        this.mockMvc.perform(
+                get("/wladder")
+                .contentType("application/json;charset=UTF-8")
+                )
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$", Matchers.contains("cat","cot","dot","dog"))));
     }
 }
