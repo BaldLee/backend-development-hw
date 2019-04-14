78a63bf9a6fe02ec68e25f4e7e0d013222e6c481
bald-win <1351533120@qq.com>
2019-04-14 10:41:35 +0800

    添加5.0.0的spring-test依赖

--- hw2/src/test/java/wordladder/WordLadderControllerTest.java
+++ hw2/src/test/java/wordladder/WordLadderControllerTest.java
@@ -1,28 +1,40 @@
 package wordladder;
 
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilderSupport.*;
 
-@RunWith(MockitoJUnitRunner.class)
-@WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration()
+@AutoConfigureMockMvc
+
 public class WordLadderControllerTest {
     protected MockMvc mockMvc;
 
     @Autowired
     protected WebApplicationContext wac;
 
-    @Before()
+    @BeforeEach()
     public void setup()
     {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();  //初始化MockMvc对象
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
+    @Test
+    @DisplayName("test1")
+    public void test1()
+    {
+        mockMvc.perform(get("/wladder").content)
+    }
 }
